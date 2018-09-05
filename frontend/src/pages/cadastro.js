86fad449625fa39e7997f455ee6c813b27df7383
Allison Sampaio <allisonsampaiox@gmail.com>
2018-09-05 19:59:45 -0300

    form login

--- frontend/src/pages/cadastro.js
+++ frontend/src/pages/cadastro.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Cadastro extends Component {
 
@@ -6,6 +7,7 @@ class Cadastro extends Component {
 		return(
 			<div>
 				<h1>Cadastro</h1>
+				Já é cadastrado? <Link to="/">Faça login.</Link>
 			</div>
 		);
 	}
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './login.css';
 
 class Login extends Component {
 
 	render() {
-		return(
+		return ( 
 			<div>
-				<h1>Login</h1>
-				<Link to="/cadastro">Cadastro</Link>
+			    <div className="text-center body">
+			        <Form className="form-signin form">
+			            <h1 className="h3 mb-3 font-weight-normal">Bem-vindo!</h1>
+			            
+			            <Input 
+			                type="email"
+			                name="email"
+			                required
+			                placeholder="Digite seu e-mail" />
+
+			            <Input type="password"
+			                name="password"
+			                required
+			                placeholder="Digite sua senha" /><br/>
+
+			            <Button className="btn btn-info btn-color margin-button" type="submit" color="primary">Entrar!</Button><br/>
+						Ainda não é cadastrado? <Link to="/cadastro">Cadastre-se!</Link>
+
+			        </Form>
+			    </div>
 			</div>
-		);
+	);
   	}
 }
 
