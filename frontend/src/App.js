8d811c38eaa7baf16ecf2fde30fb59fc0990bdb8
Pedro Perozin <perozin_pan@hotmail.com>
2018-09-06 20:31:03 -0300

    Mudei o que seria a tela principal para MainNavbar, que seria o cabeçalho principal do programa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import Login from './pages/login.js'
 import Cadastro from './pages/cadastro.js'
-<<<<<<< HEAD
 import MainNavbar from './pages/MainNavbar.js'
-=======
-import Principal from './pages/principal.js'
->>>>>>> 4abc3a61f7d34998b569fb22667304c61e61ca7a
 import './App.css';
 
 class App extends Component {
@@ -18,11 +14,7 @@ class App extends Component {
                 <Switch>
                     <Route path="/" exact={true} component={Login} />
                     <Route path="/cadastro" exact={true} component={Cadastro} />
-<<<<<<< HEAD
                     <Route path="/MainNavbar" exact={true} component={MainNavbar} />
-=======
-                    <Route path="/principal" exact={true} component={Principal} />
->>>>>>> 4abc3a61f7d34998b569fb22667304c61e61ca7a
                 </Switch> 
             </BrowserRouter>
         );
