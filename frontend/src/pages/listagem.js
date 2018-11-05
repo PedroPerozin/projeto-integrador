59b82a2b48c354058bf7ea399cd38b9894445e4d
Allison Sampaio <allisonsampaiox@gmail.com>
2018-11-05 18:17:18 -0200

    Criando listas

--- frontend/src/pages/listagem.js
+++ frontend/src/pages/listagem.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import {
-    Container, Col, Form,
-    FormGroup, Label, Input,
-    Button,
-} from 'reactstrap';
+import React from 'react';
+import { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import MainNavbar from '../componentes/MainNavbar.js'
 
 
 class Listagem extends Component {
 
     render() {
         return(
-            
+            <div>
+            	<MainNavbar/>
+            	<br/>
+
+            	<Container>
+            		<ListGroup>
+				    	<ListGroupItem>Cras justo odio</ListGroupItem>
+				    	<ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+				    	<ListGroupItem>Morbi leo risus</ListGroupItem>
+				    	<ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+				    	<ListGroupItem>Vestibulum at eros</ListGroupItem>
+			     	</ListGroup>
+            	</Container>
+            	
+            </div>
         );
     }
-
 }
 
 export default Listagem;
