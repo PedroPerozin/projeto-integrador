afabfe5151500969d6bdb67717aa52f362b3ee11
Allison Sampaio <allisonsampaiox@gmail.com>
2018-11-26 16:16:18 -0200

    Mudei o modelo de equipamento

--- models/equipment.js
+++ models/equipment.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EquipmentSchema = new Schema({
+  name: {
+    type: Schema.Types.String,
+    required: true
+  },
   description: {
     type: Schema.Types.String,
+    required: false
+  },
+  quantity: {
+    type: Schema.Types.Number,
     required: true
   }
 });
